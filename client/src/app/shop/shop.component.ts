7ad93dd1db193324447d7f4cc39d1309a5d785cf
enjoyst1ck <bwwojcik0@gmail.com>
2023-04-01 18:28:17 +0200

    Created UI/Data fetches and displays

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from './shop.service';
+import { Product } from '../shared/models/product';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
+
+  constructor(private shopService: ShopService) {}
+  
+  ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts().subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error)
+    })
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = response,
+      error: error => console.log(error)
+    })
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next: response => this.types = response,
+      error: error => console.log(error)
+    })
+  }
+}
---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/product';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/'
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products');
+  }
+
+  getBrands() {
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<Type[]>(this.baseUrl + 'products/types');
+  }
+  
+}

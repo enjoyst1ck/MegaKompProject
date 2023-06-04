13ca44ebd8a2017ca56e69114e3fbe86cf6d0173
enjoyst1ck <bwwojcik0@gmail.com>
2023-06-04 15:05:16 +0200

    Added client-side data validation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: {breadcrumb: 'Strona główna'}},
@@ -12,7 +13,13 @@ const routes: Routes = [
   { path: 'server-error', component: ServerErrorComponent},
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: {breadcrumb: 'Sklep'}},
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: {breadcrumb: 'Koszyk'}},
-  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), data: {breadcrumb: 'Składanie zamówienia'}},
+  { 
+    path: 'checkout', 
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), 
+    data: {breadcrumb: 'Składanie zamówienia'}
+  },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule), data: {breadcrumb: 'Konto'}},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,6 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { BasketItem } from 'src/app/shared/models/basket';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,7 +12,7 @@ import { BasketItem } from 'src/app/shared/models/basket';
 })
 export class NavBarComponent {
 
-  constructor(public basketService: BasketService) {}
+  constructor(public basketService: BasketService, public accountService: AccountService) {}
 
   getCount(items: BasketItem[]) {
     return items.reduce((sum, item) => sum + item.quantity, 0);
